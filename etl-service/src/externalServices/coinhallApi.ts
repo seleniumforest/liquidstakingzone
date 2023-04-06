030e729ea5899f64ae958dd241b1e1c4e63eff0f
Kirill <troninkirill@gmail.com>
2023-04-06 21:05:37 +0700

    fix field names

--- etl-service/src/externalServices/coinhallApi.ts
+++ etl-service/src/externalServices/coinhallApi.ts
@@ -4,13 +4,13 @@ import { randomUUID } from "../helpers";
 
 export const getLunaPrices = async (from: number | undefined, to: number | undefined) : Promise<Price[]> => {
     const astroLunaPoolDataUrl = `https://api.coinhall.org/api/v1/charts/terra1re0yj0j6e9v2szg7kp02ut6u8jjea586t6pnpq6628wl36fphtpqwt6l7p?bars=329&from=${from || "1665251079"}&interval=1h&quoteAsset=uluna&to=${to || Date.now()}`;
-    const response = await axios.get<{ date: number, price: number }[]>(astroLunaPoolDataUrl);
+    const response = await axios.get<{ time: number, open: number }[]>(astroLunaPoolDataUrl);
 
     return response.data.map(x => ({
         id: randomUUID(),
         coin: "terra-luna-2",
-        date: x.date,
-        price: x.price,
+        date: x.time,
+        price: x.open,
         vsCurrency: "stluna"
     }));
 }
