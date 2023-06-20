bb430facc3f3de532b19313773e1630305f3698d
Kirill Tronin <troninkirill@gmail.com>
2023-06-20 11:52:05 +0700

    cors

--- backend/index.ts
+++ backend/index.ts
@@ -6,13 +6,12 @@ import { zones } from "./constants";
 
 const app = express();
 
-const whitelist = ['https://liquidstakingzone.com', 'http://localhost:3000'];
+const whitelist = ['https://liquidstaking.zone', 'http://localhost:3000', "https://analytics-backend.vercel.app"];
 
 const corsOptionsCheck = (req: any, callback: any) => {
     let corsOptions;
 
     let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
-
     if (isDomainAllowed) {
         // Enable CORS for this request
         corsOptions = { origin: true }
