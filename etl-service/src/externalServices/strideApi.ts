3cf8336b6fe0893b1e28abe6ad325cfc78e8981d
Kirill <troninkirill@gmail.com>
2023-07-03 11:31:50 +0700

    new way to calculate assets incl interest

--- etl-service/src/externalServices/strideApi.ts
+++ etl-service/src/externalServices/strideApi.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { denomToZone } from "../helpers";
 import NodeCache from "node-cache";
 
-const hostZoneUrl = "https://stride-fleet.main.stridenet.co/api/Stride-Labs/stride/stakeibc/host_zone";
+const baseUrl = "https://stride-fleet.main.stridenet.co";
 const cache = new NodeCache({
-    stdTTL: 600  
+    stdTTL: 600
 });
 let key = "fetchZoneInfo";
 
@@ -14,11 +14,12 @@ export const fetchZonesInfo = async (): Promise<HostZoneConfig[]> => {
         return cached;
 
     try {
-        let data = (await axios.get(hostZoneUrl)).data.host_zone;
+        let data = (await axios.get(baseUrl + "/api/Stride-Labs/stride/stakeibc/host_zone")).data.host_zone;
 
         let result = data.map((zone: any) => ({
             chainId: zone.chain_id,
             zone: denomToZone(zone.host_denom),
+            hostDenom: zone.host_denom,
             prefix: zone.bech32prefix,
             address: zone.address,
             redemptionRate: Number(zone.redemption_rate),
@@ -36,6 +37,36 @@ export const fetchZonesInfo = async (): Promise<HostZoneConfig[]> => {
         return [];
     }
 }
+
+export const fetchUserRedemptionRecords = async (): Promise<UserRedemptionRecord[] | undefined> => {
+    try {
+        let nextKey;
+
+        do {
+            let url = baseUrl + "/api/Stride-Labs/stride/records/user_redemption_record?pagination.limit=1000";
+            if (nextKey)
+                url += `&pagination.key=${nextKey}`;
+            let data = await axios.get(url);
+            nextKey = data.data.pagination.next_key;
+            let result = data.data.user_redemption_record;
+
+            return result as UserRedemptionRecord[];
+        } while (nextKey)
+    }
+    catch (e: any) { console.log(`fetchUserRedemptionRecords Error ` + e?.message) }
+}
+
+export interface UserRedemptionRecord {
+    id: string,
+    sender: string,
+    receiver: string,
+    amount: string
+    denom: string,
+    host_zone_id: string,
+    epoch_number: number,
+    claim_is_pending: boolean
+}
+
 export interface HostZoneConfig {
     chainId: string,
     prefix: string,
@@ -45,5 +76,6 @@ export interface HostZoneConfig {
     feeAcc: string,
     redemptionAcc: string,
     withdrawalAcc: string,
-    redemptionRate: number
+    redemptionRate: number,
+    hostDenom: string
 }
--- etl-service/src/jobs/externalAccountsCheckerJob.ts
+++ etl-service/src/jobs/externalAccountsCheckerJob.ts
@@ -1,4 +1,4 @@
-import { fetchZonesInfo } from "../externalServices/strideApi";
+import { fetchUserRedemptionRecords, fetchZonesInfo } from "../externalServices/strideApi";
 import { NetworkManager } from "../externalServices/tendermint";
 import { timeSpans } from "../constants";
 import { Balance, insertAccountBalance } from "../db/balances";
@@ -7,27 +7,37 @@ import Big from "big.js";
 import { QueryClient, setupStakingExtension, StargateClient } from "@cosmjs/stargate";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
 import { UnbondingDelegation } from "cosmjs-types/cosmos/staking/v1beta1/staking";
+import { QueryDelegatorUnbondingDelegationsResponse } from "cosmjs-types/cosmos/staking/v1beta1/query";
 
 export const externalAccountsCheckerJob = async () => {
     console.log(`externalAccountsCheckerJob: ${new Date()}`);
 
     let zoneInfo = await fetchZonesInfo();
+    let redemeptionRecods = await fetchUserRedemptionRecords();
+    if (!redemeptionRecods || !zoneInfo)
+        return;
+
     await Promise.allSettled(zoneInfo.map(async (zone) => {
         try {
-            let zoneEndpoints = await NetworkManager.create({
-                name: prefixToRegistryName(zone.prefix)
-            });
+            let name = prefixToRegistryName(zone.prefix);
+            let zoneEndpoints = await NetworkManager.create({ name });
 
             let rpcs = await zoneEndpoints.getSyncedRpcs();
             if (rpcs.length === 0) {
-                console.warn(`externalAccountsCheckerJob: No endpoints found for prefix ${zone.prefix}`);
+                console.warn(`externalAccountsCheckerJob: No endpoints found for zone ${name}`);
                 return;
             };
 
             let staked = await getBalanceStaked(zone.delegationAcc, rpcs);
             let balance = await getBalanceOnAccount(zone.delegationAcc, staked?.denom!, rpcs)
-            //let undelegated = await getUndelegatedBalance(zone.delegationAcc, rpcs);
-            let sum = Big(staked?.amount!).plus(Big(balance?.amount!)).toFixed();
+            let undelegated = await getUndelegatedBalance(zone.delegationAcc, rpcs);
+            if (!staked || !balance || !undelegated)
+                return;
+
+            let redemeptionRecodsForZone = redemeptionRecods!.filter(x => x.denom === zone.hostDenom && x.claim_is_pending === false);
+            let redemptedAmount = redemeptionRecodsForZone.reduce((prev, cur) => prev.add(cur.amount), Big(0));
+            //console.log(`${zone.zone} balances: ` + staked?.amount!, balance?.amount!, undelegated.toFixed(), redemptedAmount.toFixed());
+            let sum = Big(staked?.amount!).plus(Big(balance?.amount!)).plus(undelegated).minus(redemptedAmount).toFixed();
 
             let balanceData: Balance = {
                 id: randomUUID(),
@@ -45,22 +55,23 @@ export const externalAccountsCheckerJob = async () => {
     console.log(`Finished update host zones transactions job: ${new Date()}`)
 };
 
-//todo pass latestKnownBalance
 const getUndelegatedBalance = async (address: string, endpoints: string[]) => {
     for (const endp of endpoints) {
         try {
             let result: UnbondingDelegation[] = [];
-            let nextKey: Uint8Array | undefined = Uint8Array.from([]);
+            let nextKey: Uint8Array | undefined;
 
             do {
                 let client = QueryClient.withExtensions(await Tendermint34Client.connect(endp), setupStakingExtension);
-                let data = await client.staking.delegatorUnbondingDelegations(address);
+                let data: QueryDelegatorUnbondingDelegationsResponse = await client.staking.delegatorUnbondingDelegations(address, nextKey);
                 nextKey = data.pagination?.nextKey;
                 result.push(...data.unbondingResponses)
-            } while ((nextKey?.length || 0) > 0);
+            } while (nextKey?.length! > 0);
 
             return result.flatMap(x => x.entries).map(x => x.balance).reduce((prev, cur) => prev.add(Big(cur)), Big(0));
-        } catch (e: any) { console.log(e?.message) }
+        } catch (e: any) { 
+            // console.log(`getUndelegatedBalance endp ${endp} Error ${e?.message}`) 
+        }
     }
 }
 
@@ -71,7 +82,9 @@ const getBalanceStaked = async (address: string, endpoints: string[]) => {
             let client = await StargateClient.connect(endp);
             let data = await client.getBalanceStaked(address);
             return data;
-        } catch (e: any) { console.log(e?.message) }
+        } catch (e: any) { 
+            // console.log(`getBalanceStaked endp ${endp} Error ${e?.message}`) 
+        }
     }
 }
 
@@ -81,7 +94,9 @@ const getBalanceOnAccount = async (address: string, denom: string, endpoints: st
             let client = await StargateClient.connect(endp);
             let data = await client.getBalance(address, denom);
             return data;
-        } catch (e: any) { console.log(e?.message) }
+        } catch (e: any) { 
+            // console.log(`getBalanceOnAccount endp ${endp} Error ${e?.message} `) 
+        }
     }
 }
 
